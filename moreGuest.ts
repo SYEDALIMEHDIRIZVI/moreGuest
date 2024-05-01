ca90796b044036093385f6e560f3106432c9d601
SYED ALI MEHDI RIZVI <s.a.m.rizvi@hotmail.com>
2024-05-01 23:00:41 +0500

    first commit

---
+++ moreGuest.ts
@@ -0,0 +1,34 @@
+//console.log('\n'+'\x1b[4m'+'a) List of invitees who will attend the dinner to night:'+'\n');
+// More Guests: You just found a bigger dinner table, so now more space is
+// available. Think of three more guests to invite to dinner.
+
+let Invitee = ['Zayghum', 'Eric', 'Jhonson','Mutahir'];
+// let Status = ['Deceased','Alive','Alive'];
+// let Invitedtodinner = ['Not invited','Invited','Invited'];
+// console.log(Invitee)
+// Invitee.pop()
+// console.log(Invitee)
+// ==================================
+    console.log(Invitee)   
+    Invitee.splice(0,0,'Ali Akber')
+   console.log(Invitee)
+   let index_count=Invitee.length/2
+   console.log('index count =',index_count)
+   Invitee.splice(2.5,0,'Mehdi')
+   console.log(Invitee)
+// ===========================================
+
+// Invitee.shift()
+// console.log(Invitee)
+// Invitee.unshift('BASIM')
+// console.log(Invitee)
+// Invitee['Jhonson']='John Sina'
+// console.log(Invitee)
+// Invitee['Jhonson']='Faizan'
+// console.log(Invitee)
+
+// for (var a=0; a<Invitee.length; a ++) {
+//     console.log(`Mr ${Invitee[a]} is cordially invited to the dinner at my home`);
+    
+
+// }
